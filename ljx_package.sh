db151422339847eb0fe0f61fe09830904c51a5e3
ljx360 <79862310+ljx360@users.noreply.github.com>
2023-06-13 21:43:33 +0800

    Update ljx_package.sh

--- ljx_package.sh
+++ ljx_package.sh
@@ -1,2 +1,12 @@
 #git small-package in feeds.conf.default
-git clone https://github.com/kenzok8/small-package package/small-package
+#git clone https://github.com/kenzok8/small-package package/small-package
+
+
+cd /workdir/openwrt
+sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+
+#git pull
+#./scripts/feeds update -a
+#./scripts/feeds install -a
+#make menuconfig
